d93795da4fd0e88edcbce05b7a7e6b04f382abff
itsrojas <t.rojas@alumno.um.edu.ar>
2023-04-25 00:03:48 -0300

    ayudanos

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+import unittest
+
+def testfibonacci(number): 
+    fibonacci_nros = [0, 1]
+    total = 1
+    print("number" + str(number))
+    for index in range(2, number + 1):
+        total += (
+            fibonacci_numbers[-2]
+        )
+    return total
+class TestFibonacci(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual
